e5df82ff2c86adfba9ea31113fb8c07023430303
innosday <imylove2007@gmail.com>
2025-03-19 08:34:49 +0900

    Update game.py

--- game.py
+++ game.py
@@ -5,73 +5,73 @@ import random
 
 class Bar:
     def __init__(self,bg,max):
-        self.max=max
-        self.var_max = max
-        self.bg = bg
+        self.__max=max
+        self.__var_max = max
+        self.__bg = bg
         
     @property
-    def vmax(self):
-        return self.var_max 
+    def var_max(self):
+        return self.__var_max 
     
     def place(self,pos,plus_x,plus_y):
-        pygame.draw.rect(self.bg,"black",[(pos[0]-self.max/2)-plus_x,pos[1]-plus_y,self.max,10])
-        pygame.draw.rect(self.bg,"red",[(pos[0]-self.max/2)-plus_x,pos[1]-plus_y,self.var_max,10])
+        pygame.draw.rect(self.__bg,"black",[(pos[0]-self.__max/2)-plus_x,pos[1]-plus_y,self.__max,10])
+        pygame.draw.rect(self.__bg,"red",[(pos[0]-self.__max/2)-plus_x,pos[1]-plus_y,self.__var_max,10])
         
     def active(self,num):
-        if self.var_max > 0:
-            self.var_max -= num
+        if self.__var_max > 0:
+            self.__var_max -= num
     
     def full_change(self):
-        self.var_max = self.max
+        self.__var_max = self.__max
 
 class Ball:
     def __init__(self,screen:pygame.Surface,coords:tuple[float,float],radius = 10):
-        self.screen = screen
-        self.x,self.y = coords
-        self.radius = radius
+        self.__screen = screen
+        self.__x,self.__y = coords
+        self.__radius = radius
     
     @property
     def coords(self):
-        return self.x,self.y
+        return self.__x,self.__y
     
     @coords.setter
     def coords(self,coords):
-        self.x,self.y = coords
+        self.__x,self.__y = coords
     
     def move(self,tick,speed,keys:pygame.key.ScancodeWrapper):
         if keys[pygame.K_w]:
-            self.y -= speed * tick
+            self.__y -= speed * tick
         if keys[pygame.K_s]:
-            self.y += speed *tick
+            self.__y += speed *tick
         if keys[pygame.K_a]:
-            self.x -= speed*tick
+            self.__x -= speed*tick
         if keys[pygame.K_d]:
-            self.x += speed * tick
+            self.__x += speed * tick
         return self
     def draw(self,color):
-        pygame.draw.circle(self.screen,color,self.coords,self.radius)
+        pygame.draw.circle(self.__screen,color,self.coords,self.__radius)
         return self
     
     def has_collision_with(self, other_ball:'Ball') -> bool:
         x, y = self.coords
         ox, oy = other_ball.coords
-        if (x-ox)**2 + (y-oy)**2 <= (self.radius + other_ball.radius)**2:
+        if (x-ox)**2 + (y-oy)**2 <= (self.__radius + other_ball.__radius)**2:
             return True
         else:
             return False
     
     def line(self,other_ball:'Ball'):
-        pygame.draw.aaline(screen,"green",other_ball.coords,self.coords,10)
+        pygame.draw.aaline(self.__screen,"green",other_ball.coords,self.coords,10)
     
     def move_to_player(self,other_ball:'Ball',distance):
         if self.coords[0] > other_ball.coords[0]:
-            self.x -= distance/random.randrange(100,450)
+            self.__x -= distance/random.randrange(100,450)
         elif self.coords[0] < other_ball.coords[0]:
-            self.x += distance/random.randrange(100,450)
+            self.__x += distance/random.randrange(100,450)
         if self.coords[1] > other_ball.coords[1]:
-            self.y -= distance/random.randrange(100,450)
+            self.__y -= distance/random.randrange(100,450)
         elif self.coords[1] < other_ball.coords[1]:
-            self.y += distance/random.randrange(100,450)
+            self.__y += distance/random.randrange(100,450)
         
 #초기화화
 pygame.init()
@@ -137,6 +137,12 @@ while running:
     if player.has_collision_with(mouse):
         player_health.active(0.5)
     
+    if player_health.var_max <= 0:
+        print("fsd")
+        # pygame.display.flip()
+        # pygame.time.delay(60000)
+        
+
     dt = clock.tick(60) / 1000
     next_bot.active(next_bot_CT)
     
